dcbe7932a4fa456be201348fd6f885526042b4f7
DAHUO-Melbourne <huodamelbourne@gmail.com>
2020-11-26 16:01:37 +1100

    build: settle main structure

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+import Homepage from './pages/homepage';
+import LoginPage from './pages/login';
+
+function App() {
+  return (
+    <Fragment>
+
+    </Fragment>
+  )
+}
+
+export default App;
---
+++ src/data/index.ts
@@ -0,0 +1,55 @@
+export const data = [
+  {
+    "key": 1,
+    "icon": "appstore",
+    "title": "软件测试科",
+    "url": "",
+    "children": [
+      {
+        "key": 4,
+        "title": "广电组",
+        "url": "",
+        "children": [
+          {
+            "key": 8,
+            "title": "客制化",
+            "url": "",
+            "children": [
+              {
+                "key": 19,
+                "title": "版本测试",
+                "url": ""
+              }
+            ]
+          }, {
+            "key": 9,
+            "title": "客供",
+            "url": ""
+          }
+        ]
+      }, {
+        "key": 5,
+        "title": "光通组",
+        "url": "",
+        "children": [
+          {
+            "key": 16,
+            "text": "版本测试",
+            "title": "",
+            "url": ""
+          }
+        ]
+      }
+    ]
+  }, {
+    "key": 2,
+    "icon": "setting",
+    "title": "硬件测试科",
+    "url": ""
+  }, {
+    "key": 3,
+    "icon": "mail",
+    "title": "EMC测试科",
+    "url": ""
+  }
+]
