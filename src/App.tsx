f34d26f375744b9c9e038ff1fac737000c3530c8
DAHUO-Melbourne <huodamelbourne@gmail.com>
2020-11-30 15:50:04 +1100

    fix: file name tricky bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react';
 import Homepage from './pages/homepage';
-import LoginPage from './pages/loginPage';
+import LoginPage from './pages/LoginPage';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 function App() {
